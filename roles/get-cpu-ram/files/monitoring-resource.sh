3a6027b215aee85b959d834a7a5255ab33a6bbfa
trandung410 <65598928+trandung410@users.noreply.github.com>
2021-09-04 14:06:52 +0700

    Update monitoring-resource.sh

--- roles/get-cpu-ram/files/monitoring-resource.sh
+++ roles/get-cpu-ram/files/monitoring-resource.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ramusage=$(free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-cpuload=$(cat /proc/loadavg |awk '{print $1}')
+cpuload=$(cat /proc/loadavg |awk '{print $3}')
 cpuusage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
 timeos=$(date +"%d/%m/%Y  %T")
 hostname=$(uname -n)
