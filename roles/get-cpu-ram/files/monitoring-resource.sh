7bdb30ca9730344e870560f0a9114fd0b639f254
trandung410 <65598928+trandung410@users.noreply.github.com>
2021-09-12 11:53:54 +0700

    Update monitoring-resource.sh

--- roles/get-cpu-ram/files/monitoring-resource.sh
+++ roles/get-cpu-ram/files/monitoring-resource.sh
@@ -4,8 +4,8 @@ cpuload=$(cat /proc/loadavg |awk '{print $1}')
 cpuusage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
 timeos=$(date +"%d/%m/%Y  %T")
 hostname=$(uname -n)
-echo "---------------------------------------" >> /root/VMware-$hostname
-echo $timeos >> /root/VMware-$hostname
-echo "CPU Load: "$cpuload >> /root/VMware-$hostname
-echo "CPU Usage: "$cpuusage >> /root/VMware-$hostname
-echo $ramusage >> /root/VMware-$hostname
+echo "---------------------------------------" >> /root/OpenStackBD-$hostname
+echo $timeos >> /root/OpenStackBD-$hostname
+echo "CPU Load: "$cpuload >> /root/OpenStackBD-$hostname
+echo "CPU Usage: "$cpuusage >> /root/OpenStackBD-$hostname
+echo $ramusage >> /root/OpenStackBD-$hostname
